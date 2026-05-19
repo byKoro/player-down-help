a4f06f056385e6bfb8de0eb46916112cf073a5f7
byKoro <yuriludwig.email@gmail.com>
2026-05-19 00:30:14 -0300

    update

--- scripts/main.js
+++
@@ -1,5 +0,0 @@
-import { DownPlayer } from './system/death_utils.js';
-import { getPlayersCaidos } from './system/player_utils.js';
-import { system, world } from '@minecraft/server';
-
-DownPlayer.init();
--- scripts/system/config.js
+++
@@ -1,5 +0,0 @@
-export class Config {
-  static tag_down = 'player.down';
-  static down_block = 'playerdown:block_down';
-  static dimensions = ['overworld', 'nether', 'the_end'];
-}
